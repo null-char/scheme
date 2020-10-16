1e4c8d1ee60d834c211dd086f079b0593e3b517f
null-char <skp.frl@gmail.com>
2020-10-17 00:26:37 +0530

    feat(LispVal): add helper function to show listvals

--- src/LispVal.hs
+++ src/LispVal.hs
@@ -2,16 +2,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module LispVal
-  (
+  ( LispVal (..),
   )
 where
 
 import Control.Monad.Reader
 import qualified Data.Map as M
-import Data.Text as T
+import qualified Data.Text as T
 import Data.Typeable (Typeable)
 
 type EnvCtx = M.Map T.Text LispVal
+
+-- Takes a list of `LispVal`s as arguments of the function
+data IFunc = IFunc {fn :: [LispVal] -> Eval LispVal}
+
+instance Eq IFunc where
+  (==) _ _ = False
 
 data LispVal
   = Atom T.Text
@@ -22,10 +28,7 @@ data LispVal
   | Lambda IFunc EnvCtx
   | Nil
   | Bool Bool
-  deriving (Typeable)
-
--- Takes a list of `LispVal`s as arguments of the function
-data IFunc = IFunc {fn :: [LispVal] -> Eval LispVal}
+  deriving (Typeable, Eq)
 
 newtype Eval a = Eval {unEval :: ReaderT EnvCtx IO a}
   deriving
@@ -35,3 +38,19 @@ newtype Eval a = Eval {unEval :: ReaderT EnvCtx IO a}
       MonadIO,
       MonadReader EnvCtx
     )
+
+showVal :: LispVal -> T.Text
+showVal v =
+  case v of
+    (Atom atom) -> atom
+    (List values) -> T.concat ["(", T.unwords $ showVal <$> values, ")"]
+    (Number n) -> T.pack $ show n
+    (String s) -> T.concat ["\"", s, "\""]
+    (Fun _) -> "(internal func)"
+    (Lambda _ _) -> "(lambda func)"
+    Nil -> "Nil"
+    (Bool True) -> "#t"
+    (Bool False) -> "#f"
+
+instance Show LispVal where
+  show = T.unpack . showVal
